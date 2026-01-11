551d910e2d196c37cfc392ad74506cf141386753
shubham33159 <shubham33159@gmailcom>
2026-01-11 20:59:49 +0530

    insert form data into the DB

--- reviews/models.py
+++ reviews/models.py
@@ -6,3 +6,6 @@ class Review(models.Model):
     user_name = models.CharField(max_length=100)
     review_text = models.TextField()
     rating = models.IntegerField()
+
+    def __str__(self):
+        return f"Username: {self.user_name}, Review_text: {self.review_text}, Rating: {self.rating}"
--- reviews/views.py
+++ reviews/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 from .forms import ReviewForm
 
+from reviews.models import Review
+
 # Create your views here.
 
 def review(request):
@@ -11,12 +13,19 @@ def review(request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            print("inside valid")
+            review = Review(
+                        user_name = form.cleaned_data["user_name"], 
+                        review_text=form.cleaned_data["review_text"], 
+                        rating=form.cleaned_data["rating"])
+            review.save()
             return HttpResponseRedirect("/thank-you")
 
     else:
         form = ReviewForm()
+        print("inside else")
 
+    print("rendering review.html")
     return render(request,"reviews/review.html",{
         "form" : form
     })
